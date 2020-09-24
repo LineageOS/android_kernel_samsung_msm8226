a5110666063cf5cb0ed851b1fff3928960ff6fc5
prototype74 <46874884+prototype74@users.noreply.github.com>
2020-09-24 16:33:16 +0200

    s3ve3g: switched to sec-battery and qpnp-charger drivers
    This commit brings up the support for sec-battery and qpnp-charger drivers to s3ve3g devices.
    Old qpnp-sec-charger driver, a heavily modified qpnp-charger driver by Samsung, caused internal issues such as no CPU deep sleep on Android 6.0+.
    
    Following nodes will be removed:
    /sys/module/qpnp_sec_charger
    
    Following nodes will be added:
    /sys/class/power_supply/pogo
    /sys/class/power_supply/ps
    /sys/class/power_supply/qpnp-chg
    /sys/class/power_supply/wireless
    /sys/module/qpnp_charger
    
    Requires Samsung's charger_monitor binary with proper permissions set to nodes (see above) at init to interact with userspace
    
    Change-Id: I1af484378b3a8dc6c3819a8670852be433131f32

---
+++ drivers/battery/s3ve3g_battery_data.h
@@ -0,0 +1,82 @@
+/*
+ * battery_data.h
+ * Samsung Mobile Battery data Header
+ *
+ *
+ * Copyright (C) 2012 Samsung Electronics, Inc.
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __SEC_BATTERY_DATA_H
+#define __SEC_BATTERY_DATA_H __FILE__
+
+static void * samsung_battery_data;
+
+#define CAPACITY_MAX			1000
+#define CAPACITY_MAX_MARGIN     50
+#define CAPACITY_MIN			0
+
+static sec_bat_adc_table_data_t temp_table[] = {
+  {26537, 800},
+  {26849, 750},
+  {27211, 700},
+  {27627, 650},
+  {28117, 600},
+  {28713, 550},
+  {29403, 500},
+  {30205, 450},
+  {31075, 400},
+  {32026, 350},
+  {33014, 300},
+  {34117, 250},
+  {35115, 200},
+  {36121, 150},
+  {37212, 100},
+  {38190, 50},
+  {39006, 0},
+  {39813, -50},
+  {40490, -100},
+  {41084, -150},
+  {41537, -200},
+};
+
+static sec_bat_adc_table_data_t chg_temp_table[] = {
+	{0, 0},
+};
+
+#define TEMP_HIGH_THRESHOLD_EVENT  600
+#define TEMP_HIGH_RECOVERY_EVENT   470
+#define TEMP_LOW_THRESHOLD_EVENT   (-50)
+#define TEMP_LOW_RECOVERY_EVENT    0
+#define TEMP_HIGH_THRESHOLD_NORMAL 600
+#define TEMP_HIGH_RECOVERY_NORMAL  470
+#define TEMP_LOW_THRESHOLD_NORMAL  (-50)
+#define TEMP_LOW_RECOVERY_NORMAL   0
+#define TEMP_HIGH_THRESHOLD_LPM    600
+#define TEMP_HIGH_RECOVERY_LPM     470
+#define TEMP_LOW_THRESHOLD_LPM     (-50)
+#define TEMP_LOW_RECOVERY_LPM      0
+
+#if defined(CONFIG_BATTERY_SWELLING)
+#define BATT_SWELLING_HIGH_TEMP_BLOCK			500
+#define BATT_SWELLING_HIGH_TEMP_RECOV			450
+#define BATT_SWELLING_LOW_TEMP_BLOCK			50
+#define BATT_SWELLING_LOW_TEMP_RECOV			100
+#define BATT_SWELLING_HIGH_CHG_CURRENT			0
+#define BATT_SWELLING_LOW_CHG_CURRENT			0
+#define BATT_SWELLING_DROP_FLOAT_VOLTAGE		4200
+#define BATT_SWELLING_HIGH_RECHG_VOLTAGE		4150
+#define BATT_SWELLING_LOW_RECHG_VOLTAGE			4050
+#endif
+
+#endif /* __SEC_BATTERY_DATA_H */
